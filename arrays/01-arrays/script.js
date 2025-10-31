264dca8217963e604ef9e6d60892d0184a638fa3
simonestrinati <simone.strinati@outlook.it>
2025-10-31 03:11:43 +0100

    esercizio-2-arrays

--- arrays/01-arrays/script.js
+++ arrays/01-arrays/script.js
@@ -16,6 +16,8 @@ const fourthTeacher = teachers.at(4);
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[5] = "Patrick";
+console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
